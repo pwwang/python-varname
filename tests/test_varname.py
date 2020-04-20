3b92346300efdfef99aa003bf502b35f2ed50456
pwwang <pwwang@pwwang.com>
2020-04-20 12:57:42 -0500

    Adopt executing for fulfill some limitations previous version had

--- tests/test_varname.py
+++ tests/test_varname.py
@@ -1,151 +1,195 @@
 import pytest
-from varname import varname
+from varname import varname, VarnameAssignedToInvalidVariable, MultipleTargetAssignmentWarning
 
 def test_function():
 
-	def function():
-		return varname()
+    def function():
+        return varname()
 
-	func = function()
-	assert func == 'func'
+    func = function()
+    assert func == 'func'
 
-	func = function(
-	)
-	assert func == 'func'
+    func = function(
+    )
+    assert func == 'func'
 
-	func = \
-		function()
-	assert func == 'func'
+    func = \
+        function()
+    assert func == 'func'
 
-	func = function \
-		()
-	assert func == 'func'
+    func = function \
+        ()
+    assert func == 'func'
 
-	func = [function()]
-	assert func == ['var_0']
+    func = (function
+            ())
+    assert func == 'func'
 
-def test_function_debug(caplog):
-	def function():
-		return varname(debug = True)
-	func = function()
-	assert '- Handing case:' in caplog.text
-	assert '(where varname() was called)' in caplog.text
-	assert 'Desired function/class was called in:' in caplog.text
-	assert 'Looking for where exactly' in caplog.text
-	assert 'Found at' in caplog.text
+    func = [function()]
+    assert func == ['func']
+
+    func = [function(), function()]
+    assert func == ['func', 'func']
+
+    func = (function(), )
+    assert func == ('func', )
+
+    func = (function(), function())
+    assert func == ('func', 'func')
 
 def test_function_context():
 
-	def function(*args):
-		return varname(context = 3)
+    def function(*args):
+        return varname()
 
-	func = function(
-		1, # I
-		2, # have
-		3, # a
-		4, # long
-		5, # argument
-		6, # list
-	)
-	assert func == 'var_1'
+    func = function(
+        1, # I
+        2, # have
+        3, # a
+        4, # long
+        5, # argument
+        6, # list
+    )
+    assert func == 'func'
 
-	def function(*args):
-		return varname(context = 20)
+    def function(*args):
+        return varname()
 
-	func = function(
-		1, # I
-		2, # have
-		3, # a
-		4, # long
-		5, # argument
-		6, # list
-	)
-	assert func == 'func'
+    func = function(
+        1, # I
+        2, # have
+        3, # a
+        4, # long
+        5, # argument
+        6, # list
+    )
+    assert func == 'func'
 
 def test_function_deep():
 
-	def function():
-		# I know that at which stack this will be called
-		return varname(caller = 3)
+    def function():
+        # I know that at which stack this will be called
+        return varname(caller = 3)
 
-	def function1():
-		return function()
+    def function1():
+        return function()
 
-	def function2():
-		return function1()
+    def function2():
+        return function1()
 
-	func = function2()
-	assert func == 'func'
+    func = function2()
+    assert func == 'func'
 
 def test_class():
 
-	class Klass:
-		def __init__(self):
-			self.id = varname()
-		def copy(self):
-			return varname()
+    class Klass:
+        def __init__(self):
+            self.id = varname()
+        def copy(self):
+            return varname()
 
-	k = Klass()
-	assert k.id == 'k'
+    k = Klass()
+    assert k.id == 'k'
 
-	k2 = k.copy()
-	assert k2 == 'k2'
+    k2 = k.copy()
+    assert k2 == 'k2'
 
 def test_class_deep():
 
-	class Klass:
-		def __init__(self):
-			self.id = self.some_internal()
+    class Klass:
+        def __init__(self):
+            self.id = self.some_internal()
 
-		def some_internal(self):
-			return varname(caller = 2)
+        def some_internal(self):
+            return varname(caller = 2)
 
-		def copy(self):
-			return self.copy_id()
+        def copy(self):
+            return self.copy_id()
 
-		def copy_id(self):
-			return self.copy_id_internal()
+        def copy_id(self):
+            return self.copy_id_internal()
 
-		def copy_id_internal(self):
-			return varname(caller = 3)
+        def copy_id_internal(self):
+            return varname(caller = 3)
 
-	k = Klass()
-	assert k.id == 'k'
+    k = Klass()
+    assert k.id == 'k'
 
-	k2 = k.copy()
-	assert k2 == 'k2'
+    k2 = k.copy()
+    assert k2 == 'k2'
 
 def test_false():
 
-	def func(**kwargs):
-		return varname()
+    def func(**kwargs):
+        return varname()
 
-	x = func(
-		y = func()
-	)
-	assert x == 'y'
+    x = func(
+        y = func()
+    )
+    assert x == 'x'
 
-	def func(**kwargs):
-		return varname()
+    def func(**kwargs):
+        return varname()
 
-	x = func(y = func())
-	assert x == 'x'
+    x = func(y = func())
+    assert x == 'x'
 
 def test_referring():
 
-	def Proc(**kwargs):
-		return varname()
+    def Proc(**kwargs):
+        return varname()
 
-	def func2():
-		pProc1 = Proc(
-			input = {'infile:file': ['infile']},
-			output = 'outfile:file:{{i.infile|__import__("pathlib").Path|.stem}}.txt',
-			script = """cat {{i.infile}} > {{o.outfile}}""")
-		pProc2 = Proc(
-			input = 'infile:file',
-			output = 'outfile:file:{{i.infile|__import__("pathlib").Path|.stem}}.txt',
-			script = """echo world >> {{o.outfile}}""",
-			depends = pProc1)
-		return pProc1, pProc2
+    def func2():
+        pProc1 = Proc(
+            input = {'infile:file': ['infile']},
+            output = 'outfile:file:{{i.infile|__import__("pathlib").Path|.stem}}.txt',
+            script = """cat {{i.infile}} > {{o.outfile}}""")
+        pProc2 = Proc(
+            input = 'infile:file',
+            output = 'outfile:file:{{i.infile|__import__("pathlib").Path|.stem}}.txt',
+            script = """echo world >> {{o.outfile}}""",
+            depends = pProc1)
+        return pProc1, pProc2
 
-	assert func2() == ('pProc1', 'pProc2')
+    assert func2() == ('pProc1', 'pProc2')
+
+def test_only_one():
+
+    def function():
+        return varname()
+
+    with pytest.raises(VarnameAssignedToInvalidVariable):
+        x, y = function()
+    with pytest.raises(VarnameAssignedToInvalidVariable):
+        x, y = function(), function()
+    with pytest.raises(VarnameAssignedToInvalidVariable):
+        [x] = function()
+
+
+def test_multiple_targets():
+
+    def function():
+        return varname()
+
+    with pytest.warns(MultipleTargetAssignmentWarning):
+        y = x = function()
+    assert y == x == 'y'
+
+def test_unusual():
+
+    def function():
+        return varname()
+
+    # something ridiculous
+    xyz = function()[-1:]
+    assert xyz == 'z'
+
+
+    x = 'a'
+    x += function()
+    assert x == 'avar_0'
+
+    func = function
+    x = func()
+    assert x == 'x'
+
--- varname.py
+++ varname.py
@@ -1,70 +1,51 @@
 """Get the variable name that assigned by function/class calls"""
-import re
+import ast
 import inspect
-import logging
+import warnings
+import executing
 
-__version__ = "0.0.3"
+__version__ = "0.1.0"
 
-VARNAME_INDEX = -1
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
-if not LOGGER.handlers:
-    _handler = logging.StreamHandler()  # pylint: disable=invalid-name
-    _handler.setFormatter(
-        logging.Formatter('[%(asctime)s %(name)s] %(message)s'))
-    LOGGER.addHandler(_handler)
+VARNAME_INDEX = [-1]
 
+class VarnameAssignedToInvalidVariable(Exception):
+    """If left-hand side is not a variable"""
 
-def varname(context=20, caller=1, debug=False):
-    """Get the variable name
+class MultipleTargetAssignmentWarning(Warning):
+    """When multiple-target assignment found, i.e. y = x = func()"""
+
+class UnableToRetrieveVarnameWarning(Warning):
+    """When multiple-target assignment found, i.e. y = x = func()"""
+
+def varname(caller=1):
+    """Get the variable name that assigned by function/class calls
     @params:
-        context (int): The max stacks to retrieve
-        stack (int): Which stack that the function/class will be called
-        lookback (int): How many lines should we look back for the call
+        caller (int): The call stack index
     @returns:
-        (str): The variable name
+        (str): The variable name, or `var_<index>` if failed
     """
-    global VARNAME_INDEX  # pylint:disable=global-statement
+    frame = inspect.stack()[caller+1].frame
+    node = executing.Source.executing(frame).node
+    while hasattr(node, 'parent'):
+        node = node.parent
 
-    stacks = inspect.stack(context)[:(caller + 2)]
-    # where varname() is called
-    stack_me = stacks[caller]
-    if debug:
-        LOGGER.info('- Handing case: %s: at line %s', stack_me.filename,
-                    stack_me.lineno)
-        LOGGER.info('  (where varname() was called)')
-    # where the function/class is called
-    caller = stacks[caller + 1]
-    if debug:
-        LOGGER.info('  Desired function/class was called in: %s: at line %s',
-                    caller.filename, caller.lineno)
-    # if I called in a class' __init__?
-    function = stack_me.function
-    # find the class name
-    if function == '__init__':
-        function = stack_me.frame.f_locals['self'].__class__.__name__
+        if isinstance(node, ast.Assign):
+            # Need to actually check that there's just one
+            if len(node.targets) > 1:
+                warnings.warn("Multiple targets in assignment, variable name "
+                              "on the very left will be used.",
+                              MultipleTargetAssignmentWarning)
+            target = node.targets[0]
 
-    if debug:
-        LOGGER.info('  Looking for where exactly %r was called ...', function)
-        LOGGER.info('  Code context (index = %s):', caller.index)
-        codelen = len(str(len(caller.code_context)))
-        for i, code in enumerate(caller.code_context):
-            LOGGER.info('  %s. %s', str(i).rjust(codelen), code.rstrip())
-    for i in reversed(range(caller.index + 1)):
-        line = caller.code_context[i]
-        if debug:
-            LOGGER.info('  Searching %d: %s', i, line.rstrip())
-        if '=' not in line or function not in line:
-            # it not something like 'a = func()'
-            continue
-        match = re.search(
-            r'^\s*([\w_]+)\s*=\s*(?:\w+\.)*' + function + r'\s*(?:\(|$)', line)
-        if not match:
-            continue
-        if debug:
-            LOGGER.info('  Found at %r', line)
-        return match.group(1)
-    if debug:
-        LOGGER.info('  Not found.')
-    VARNAME_INDEX += 1
-    return 'var_{}'.format(VARNAME_INDEX)
+            # Need to check that it's a variable
+            if isinstance(target, ast.Name):
+                return target.id
+
+            raise VarnameAssignedToInvalidVariable(
+                f"Invaid variable assigned: {ast.dump(target)!r}"
+            )
+
+    VARNAME_INDEX[0] += 1
+    warnings.warn(f"var_{VARNAME_INDEX[0]} used.",
+                  UnableToRetrieveVarnameWarning)
+    return f"var_{VARNAME_INDEX[0]}"
