041dfba1cfffe131166698d354d16f3fba75e5ed
pwwang <pwwang@pwwang.com>
2021-08-10 15:57:09 -0700

    Update versionand change log for 0.7.3

--- setup.py
+++ setup.py
@@ -24,10 +24,11 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='varname',
-    version='0.7.2',
+    version='0.7.3',
     description='Dark magics about variable names in python.',
     python_requires='==3.*,>=3.6.0',
-    project_urls={"homepage": "https://github.com/pwwang/python-varname", "repository": "https://github.com/pwwang/python-varname"},
+    project_urls={"homepage": "https://github.com/pwwang/python-varname",
+                  "repository": "https://github.com/pwwang/python-varname"},
     author='pwwang',
     author_email='pwwang@pwwang.com',
     license='MIT',
